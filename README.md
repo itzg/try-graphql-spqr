7a485afed075ced7e3e016f7e9ec46b3bc9bdf00
Geoff Bourne <itzgeoff@gmail.com>
2018-12-31 08:50:53 -0600

    Add example query to the README Add docs to the RootQuery Verify CompletableFuture as a response type

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 This application uses [graphql-spqr](https://github.com/leangen/graphql-spqr) with its
-[Spring Boot support](https://github.com/leangen/graphql-spqr-spring-boot-starter) and integrates Spring Security to authenticate callers.
+[Spring Boot support](https://github.com/leangen/graphql-spqr-spring-boot-starter) and 
+integrates Spring Security to authenticate callers.
 
+# Example Queries
+
+With the default login of user/testing, the following curl operation can be used to query
+the 
+
+```
+curl --request POST \
+  --url http://localhost:8080/graphql \
+  --header 'authorization: Basic dXNlcjp0ZXN0aW5n' \
+  --header 'content-type: application/json' \
+  --data '{"query":"{\n  entry(id:\"1\") {\n    id\n    owner\n    when\n    tags\n  }\n}"}'
+```
+
+or GraphQL form is:
+
+```graphql
+{
+  entry(id:"1") {
+    id
+    owner
+    when
+    tags
+  }
+}
+```
+
+which will give a JSON response like:
+
+```json
+{
+  "data": {
+    "entry": {
+      "id": "1",
+      "owner": "user",
+      "when": "2018-12-27T16:12:05.440Z",
+      "tags": [
+        "red",
+        "green",
+        "blue"
+      ]
+    }
+  }
+}
+```
+
+The response is contrived, but derived from the query to verify the operation:
+- `id` is always the one given in the query parameter
+- `owner` is the Spring Security authenticated username
+- `when` is always the current date/time
+- `tags` always contains the values red, green, blue
--- src/main/java/me/itzg/tryspqr/api/RootQuery.java
+++ src/main/java/me/itzg/tryspqr/api/RootQuery.java
@@ -8,25 +8,43 @@ import java.security.Principal;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import me.itzg.tryspqr.model.Entry;
 import org.springframework.stereotype.Service;
 
 /**
- * @author Geoff Bourne
- * @since Dec 2018
+ * For this example application, this serves as the "entry point" or root query.
  */
 @Service
 @GraphQLApi
 public class RootQuery {
 
+  /**
+   * Tries out the following capabilities:
+   * <ul>
+   *   <li>Injecting the authenticated user from our
+   *     {@link me.itzg.tryspqr.services.AuthenticatedUserArgumentInjector}</li>
+   *   <li>Declaring id as a required argument.
+   *     Schema-wise this will generate as <code>entry(id:String!) { }</code></li>
+   *   <li>Showing that the response can be wrapped in a {@link CompletableFuture}, which is what
+   *     some data stores, like jetcd, provide for async queries</li>
+   * </ul>
+   */
   @GraphQLQuery
-  public Entry entry(Principal principal, @GraphQLNonNull String id) {
-    return new Entry()
-        .setOwner(principal.getName())
-        .setWhen(new Date())
-        .setId(id);
+  public CompletableFuture<Entry> entry(Principal principal, @GraphQLNonNull String id) {
+    return
+        CompletableFuture.completedFuture(
+            new Entry()
+                .setOwner(principal.getName())
+                .setWhen(new Date())
+                .setId(id)
+        );
   }
 
+  /**
+   * Tries out the ability to augment a response object, {@link Entry} in this case, with an
+   * additional field.
+   */
   @GraphQLQuery
   public List<String> tags(@GraphQLContext Entry entry) {
     return Arrays.asList("red", "green", "blue");
